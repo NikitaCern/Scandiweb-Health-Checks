3b194f3c193ca9bb99a1bdd04e9113a747329766
Nikita Cernisovs <nikita.cernisovs@gmail.com>
2022-02-10 00:12:20 +0200

    Support for differing FE and BE urls

--- checks/security.py
+++ checks/security.py
@@ -5,11 +5,11 @@ import json
 import ssl, socket
 from print_helper import category, color, show_error, dark
 
-def check_security(url):
+def check_security(FE_url, BE_url):
     print(category("Checking security:"))
-    magento_admin_url(url)
-    sensitive_urls(url)
-    SSL_expiration(url)
+    magento_admin_url(FE_url, BE_url)
+    sensitive_urls(FE_url, BE_url)
+    SSL_expiration(FE_url)
     admin_users()
     non_commited_changes()
 
@@ -24,7 +24,7 @@ def get_urls_to_check(filename):
         show_error()
     return urls
 
-def magento_admin_url(url):
+def magento_admin_url(FE_url, BE_url):
     print("\tMagento admin url:")
 
     filepath = 'admin_urls.txt'
@@ -35,17 +35,26 @@ def magento_admin_url(url):
             f"{filepath} is empty!",
             "red"))
         return
+    if FE_url:
+        for item in danger_list:
+            temp_url = FE_url + item
+            try:
+                request = requests.get(temp_url)
+                if request.status_code == 200:
+                    print(f"\t {color(request, 'red')} {request.url}")
+            except:
+                show_error()
+    if BE_url:
+        for item in danger_list:
+            temp_url = BE_url + item
+            try:
+                request = requests.get(temp_url)
+                if request.status_code == 200:
+                    print(f"\t {color(request, 'red')} {request.url}")
+            except:
+                show_error()
 
-    for item in danger_list:
-        temp_url = url + item
-        try:
-            request = requests.get(temp_url)
-            if request.status_code == 200:
-                print(f"\t {color(request, 'red')} {request.url}")
-        except:
-            show_error()
-
-def sensitive_urls(url):
+def sensitive_urls(FE_url, BE_url):
     print("\tSensitive urls:")
 
     filepath = 'sensitive_urls.txt'
@@ -57,16 +66,29 @@ def sensitive_urls(url):
             "red"))
         return
 
-    for item in danger_list:
-        temp_url = url + item
-        try:
-            r = requests.get(temp_url)
-            if r.status_code == 200:
-                print(f"\t {color(r, 'red')} {r.url}")
-            else:
-                print(f"\t {color(r, 'green')} {r.url}")
-        except:
-            show_error()
+    if FE_url:
+        for item in danger_list:
+            temp_url = FE_url + item
+            try:
+                r = requests.get(temp_url)
+                if r.status_code == 200:
+                    print(f"\t {color(r, 'red')} {r.url}")
+                else:
+                    print(f"\t {color(r, 'green')} {r.url}")
+            except:
+                show_error()
+    
+    if BE_url:
+        for item in danger_list:
+            temp_url = BE_url + item
+            try:
+                r = requests.get(temp_url)
+                if r.status_code == 200:
+                    print(f"\t {color(r, 'red')} {r.url}")
+                else:
+                    print(f"\t {color(r, 'green')} {r.url}")
+            except:
+                show_error()
 
 
 def SSL_expiration(url):
--- run.py
+++ run.py
@@ -6,15 +6,23 @@ from print_helper import bold, color, print_header, dark
 if __name__ == '__main__':
     colorama.init()
     print_header()
-    print(bold("Enter the url ("), end="")
+    print(bold("Enter the FE url ("), end="")
     print(dark("https://www.example.com/"), end="")
     print(bold(") of the project you want to check: "), end="")
 
     pattern = re.compile('^http["s"]?:\/\/www[.]([^\/]*)[.]([^\/]*)\/$')
-    url = input().strip()
+    FE_url = input().strip()
 
-    while pattern.fullmatch(url) is None:
+    while pattern.fullmatch(FE_url) is None:
         print(f"Make sure the URL is similar to this: {color('https://www.example.com/ : ', 'red')}", end="")
-        url = input().strip()
+        FE_url = input().strip()
 
-    check_health(url)
+    print(bold("Enter the BE url (if its the same, or you dont care about it, press enter) "), end="")
+
+    BE_url = input().strip()
+    if BE_url:
+        while pattern.fullmatch(BE_url) is None:
+            print(f"Make sure the URL is similar to this: {color('https://www.example.com/ : ', 'red')}", end="")
+            BE_url = input().strip()
+
+    check_health(FE_url, BE_url)
